c4d4043ae2120f80f5fd36fb95533c270bc59912
Cade Edney <cade@intercotradingco.com>
2025-12-03 10:05:37 -0600

    Add base and day 3

---
+++ day3/main.py
@@ -0,0 +1,14 @@
+from util.main import parse_file_to_lines
+
+test_file = "dayX/test.txt"
+input_file = "dayX/input.txt"
+lines: list[str] = parse_file_to_lines(test_file)
+
+def part_1():
+    pass
+
+def part_2():
+    pass
+
+if __name__ == "__main__":
+    pass
